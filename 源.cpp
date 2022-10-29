e74aa4033ee576188810d9b2286927fc104411a0
爱莉希雅(Elysia) <115752119+Elysia3344@users.noreply.github.com>
2022-10-29 22:25:08 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,18 +1,38 @@
-#include <stdio.h>
-int main(int argc,char *argv[])
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//void loveYou(int n)//n为问题规模
+//{
+//    int i = 1;
+//    while (i <= n)
+//    {
+//        i++;
+//        printf("I love you %d\n", i);
+//
+//    }
+//    printf("I love you more than %d\n", n);
+//}
+//
+void loveyou(int flag[], int n)
 {
-	int intA,intB,intSum;
-	scanf_s("%d %d",&intA,&intB);
-	intSum=intA+intB;
-	printf("%d",intSum);
-	
-	return 0;
+    printf("djwedw\n");
+    for (int i = 0; i < n; i++)
+    {
+        if (flag[i] == n)
+        {
+            printf("hfewhfwj%d\n", n);
+            break;
+        }
+    }
 }
 
+int main()
+{
+    int flag[n] = { 1...n };
+    loveyou(flag, n);
+   /* loveyou(3000);*/
+    /*short age = 20;
+    printf("%d\n", sizeof(int));
 
-
-
-
-
-
-
+    float weight = 96.5f;*/
+    return 0;
+}
