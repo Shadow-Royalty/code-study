528404f633272f8d278fc51026d2dcf786e04bf9
爱莉希雅(Elysia) <115752119+Elysia3344@users.noreply.github.com>
2022-10-31 22:35:42 +0800

    Add files via upload

--- 2.c
+++ 2.c
@@ -1,47 +1,15 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-int Add(int x, int y)
-{
-	int z = 0;
-		z = x + y;
-
-		return z;
-
-
+int pivotIndex(int* nums, int numsSize) {
+    int total = 0;
+    for (int i = 0; i < numsSize; ++i) {
+        total += nums[i];
+    }
+    int sum = 0;
+    for (int i = 0; i < numsSize; ++i) {
+        if (2 * sum + nums[i] == total) {
+            return i;
+        }
+        sum += nums[i];
+    }
+    return -1;
 }
 
-
-
-
-
-
-int main()
-{
-	int a = 10;
-	int b = 20;
-	int sum = Add(a, b);
-	/*int a = 10;
-	int b = 20;
-	int max = 0;
-	max = (a > b ? a : b);
-	printf("%d\n", max);*/
-	//一样是编译器问题
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	return 0;
-
-
-
-}
